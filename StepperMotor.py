58b3a9f6f7cb24744d1749d27207350ffa72ce89
Bart Chrzaszcz <bart.chr@gmail.com>
2017-01-06 22:27:55 -0500

    Commented commands out. Stepper motor just spins 2 revolutions

--- StepperMotor.py
+++ StepperMotor.py
@@ -34,7 +34,7 @@ myStepper = mh.getStepper(200, 1)       # 200 steps/rev, motor port #1
 
 myStepper.setSpeed(30)                  # 30 RPM
 
-
+myStepper.step(400, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.DOUBLE)
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #***JSON***
@@ -42,9 +42,9 @@ myStepper.setSpeed(30)                  # 30 RPM
 #with open('winStat.json', 'w+') as f:
 #    f.write(json.dumps(stepsMoved))
 
-with open('winStat.json', 'r') as f:
-    stepsMoved = json.loads(f.read())
-    print stepsMoved['position']
+#with open('winStat.json', 'r') as f:
+#    stepsMoved = json.loads(f.read())
+#    print stepsMoved['position']
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #def stepper_worker(stepper, numsteps, direction, style):
 #        print("Steppin!")
@@ -65,14 +65,14 @@ with open('winStat.json', 'r') as f:
 
 # temp commands
 
-if stepsMoved['position'] == 0:
-	print "Window is closed all the way at the moment.\n"
+#if stepsMoved['position'] == 0:
+#	print "Window is closed all the way at the moment.\n"
 
 
-elif temp_c>15 or temp_c==15:
-		print "It's nice outside! \n Window is opening."
-		myStepper.step(400, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.DOUBLE)
+#elif temp_c>15 or temp_c==15:
+#		print "It's nice outside! \n Window is opening."
+#		myStepper.step(400, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.DOUBLE)
 
-		stepsMoved = {'position': 1}
-                with open('winStat.json', 'w+') as f:
-                	f.write(json.dumps(stepsMoved))
+#		stepsMoved = {'position': 1}
+#                with open('winStat.json', 'w+') as f:
+#                	f.write(json.dumps(stepsMoved))
